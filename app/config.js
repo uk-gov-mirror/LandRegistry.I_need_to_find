963942dc3e76449ddfa0abd945c346a0edcadcf0
Erin <erin.tregunna1@gmail.com>
2019-06-07 12:02:26 +0100

    some content updates - 'planning application for property', content hierarchy looked at, service name updated

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'I need to find property information',
+  serviceName: 'Find out about land or property',
 
   // Default port that prototype runs on
   port: '3000',
