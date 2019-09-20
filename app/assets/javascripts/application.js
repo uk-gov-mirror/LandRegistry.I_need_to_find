0eb7741afc681ff43191aa3e5128c14ec1632da9
Erin <erin.tregunna1@gmail.com>
2019-09-20 12:10:34 +0100

    date format fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -91,8 +91,11 @@ $(document).ready(function () {
     $('.app-house-price-index-authority').text('Property price trends for Plymouth');
     $('.app-property-type').text('Terraced');
     $('.app-price-paid-list').html('<b>&pound;107,000</b> on 26 September 2008<br>');
+    $('.app-price-paid-list-w').html('<b>&pound;107,000</b> ar 26 September 2008<br>');
     $('.app-price-paid-list2').html('<b>&pound;280,000</b> on 15 July 2010<br>');
+    $('.app-price-paid-list2-w').html('<b>&pound;280,000</b> ar 15 July 2010<br>');
     $('.app-price-paid-list3').html('<b>&pound;117,000</b> on 15 July 2010<br>');
+    $('.app-price-paid-list3-w').html('<b>&pound;117,000</b> ar 15 July 2010<br>');
     $('.app-price-paid-link').attr('href', 'http://landregistry.data.gov.uk/app/ppd/search?et%5B%5D=lrcommon%3Afreehold&et%5B%5D=lrcommon%3Aleasehold&limit=100&nb%5B%5D=true&nb%5B%5D=false&paon=8&ptype%5B%5D=lrcommon%3Adetached&ptype%5B%5D=lrcommon%3Asemi-detached&ptype%5B%5D=lrcommon%3Aterraced&ptype%5B%5D=lrcommon%3Aflat-maisonette&ptype%5B%5D=lrcommon%3AotherPropertyType&street=Peel+Street&tc%5B%5D=ppd%3AstandardPricePaidTransaction&tc%5B%5D=ppd%3AadditionalPricePaidTransaction&town=Plymouth');
     $('.app-percentage-index-change').text('+3.2%');
     $('.app-house-price-index-information').attr('href', 'http://landregistry.data.gov.uk/app/ukhpi/browse?from=2018-03-01&location=http%3A%2F%2Flandregistry.data.gov.uk%2Fid%2Fregion%2Fcity-of-plymouth&to=2019-03-01');
@@ -114,6 +117,7 @@ $(document).ready(function () {
     $('.app-house-price-index-authority').text('Property price trend for Exeter');
     $('.app-property-type').text('Terraced');
     $('.app-price-paid-list').html('<b>&pound;161,000</b> on 26 August 2011<br>');
+    $('.app-price-paid-list-w').html('<b>&pound;161,000</b> ar 26 August 2011<br>');
     $('.app-price-paid-link').attr('href', 'http://landregistry.data.gov.uk/app/ppd/search?et%5B%5D=lrcommon%3Afreehold&et%5B%5D=lrcommon%3Aleasehold&limit=100&nb%5B%5D=true&nb%5B%5D=false&paon=3&ptype%5B%5D=lrcommon%3Adetached&ptype%5B%5D=lrcommon%3Asemi-detached&ptype%5B%5D=lrcommon%3Aterraced&ptype%5B%5D=lrcommon%3Aflat-maisonette&ptype%5B%5D=lrcommon%3AotherPropertyType&street=Clinton+Street&tc%5B%5D=ppd%3AstandardPricePaidTransaction&tc%5B%5D=ppd%3AadditionalPricePaidTransaction&town=Exeter');
     $('.app-percentage-index-change').text('+1%');
     $('.app-house-price-index-information').attr('href', 'http://landregistry.data.gov.uk/app/ukhpi/browse?from=2018-03-01&location=http%3A%2F%2Flandregistry.data.gov.uk%2Fid%2Fregion%2Fexeter&to=2019-03-01');
@@ -131,6 +135,7 @@ $(document).ready(function () {
     $('.app-house-price-index-authority').text('Property price trend for City Bristol');
     $('.app-property-type').text('Semi detached');
     $('.app-price-paid-list').html('<b>&pound;582,500</b> on 10 September 2009<br>');
+    $('.app-price-paid-list-w').html('<b>&pound;582,500</b> ar 10 September 2009<br>');
     $('.app-price-paid-link').attr('href', 'http://landregistry.data.gov.uk/app/ppd/search?et%5B%5D=lrcommon%3Afreehold&et%5B%5D=lrcommon%3Aleasehold&limit=100&nb%5B%5D=true&nb%5B%5D=false&paon=10&ptype%5B%5D=lrcommon%3Adetached&ptype%5B%5D=lrcommon%3Asemi-detached&ptype%5B%5D=lrcommon%3Aterraced&ptype%5B%5D=lrcommon%3Aflat-maisonette&ptype%5B%5D=lrcommon%3AotherPropertyType&street=Elgin+Park&tc%5B%5D=ppd%3AstandardPricePaidTransaction&tc%5B%5D=ppd%3AadditionalPricePaidTransaction&town=Bristol');
     $('.app-percentage-index-change').text('+1.3%');
     $('.app-house-price-index-information').attr('href', 'http://landregistry.data.gov.uk/app/ukhpi/browse?from=2018-03-01&location=http%3A%2F%2Flandregistry.data.gov.uk%2Fid%2Fregion%2Fcity-of-bristol&to=2019-03-01');
