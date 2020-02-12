dacb86a61ea3cb70d8c726e187a937ff1553e1c8
matttomas <matthew.tomas@landregistry.gov.uk>
2020-02-12 14:00:33 +0000

    fixing tech issues' f

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/01-2020/dataClean', function (req, res) {
+
+  req.session.data = {}
+  res.redirect('search-by-postcode-or-street-name')
+})
 
 module.exports = router
