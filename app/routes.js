67cfa5d98b94ac4c4095898e11c28be98eb53e74
Erin <erin.tregunna1@gmail.com>
2021-02-24 11:09:33 +0000

    routes file update and index page placeholder for round 13 commented out for now

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,47 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
 // Add your routes here - above the module.exports line
+router.use('/node_modules', express.static('node_modules'))
+
+router.post('/deeds-OC2-1/test', function (req, res) {
+
+  let regAnswer = req.session.data['docsReg']
+
+  if (regAnswer === 'yes-docs') {
+    res.redirect('/deeds-OC2-1/request-deeds-not-in-reg')
+  }
+  if (regAnswer === 'no-docs') {
+    res.redirect('/deeds-OC2-1/check-doc-information')
+  }
+})
+
+router.post('/deeds-OC2-2/test2', function (req, res) {
+
+  let regAnswer = req.session.data['docsReg2']
+
+  if (regAnswer === 'yes-docs') {
+    res.redirect('/deeds-OC2-2/request-deeds-not-in-reg')
+  }
+  if (regAnswer === 'no-docs') {
+    res.redirect('/deeds-OC2-2/check-doc-information-not-requested')
+  }
+})
+
+router.post('/deeds-OC2-3/test3', function (req, res) {
+
+  let regAnswer = req.session.data['docsReg3']
+
+  if (regAnswer === 'yes-docs') {
+    res.redirect('/deeds-OC2-3/request-deeds-not-in-reg')
+  }
+  if (regAnswer === 'no-docs') {
+    res.redirect('/deeds-OC2-3/check-doc-information-without-requested')
+  }
+})
+
+
+
 
 module.exports = router
